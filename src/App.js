80b263786cdcb739905b2bff42445b96b86eb51b
Dnavarro88 <42564706+Dnavarro88@users.noreply.github.com>
2021-07-21 21:32:23 -0700

    Added CardList & Card Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -7,32 +7,23 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 'asc1'
-        },
-        {
-          name: 'Dracula',
-          id: 'as1w'
-        },
-        {
-          name: 'Zombie',
-          id: 'as1r2'
-        }
-      ]
+      monsters: [] 
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }));
   }
 
 
   render() {
     return (
-      <div className="App">
-        {
-          this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name} </h1>)
-        }
-    </div>
-    )
+      <div className='App'>
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
   }
 }
 
